97cd2aa310f7383c125b1147ff5705eefef42a7d
Olyxz16 <cedric.sazos@tutanota.com>
2025-11-19 10:43:07 +0100

    2

--- tp1/1.py
+++ tp1/1.py
@@ -3,10 +3,21 @@ import matplotlib.pyplot as plt
 import random
 import os
 
-mean = np.array([0,0])
-cov = np.array([[1,0.5],[0.5,1]])
-X = (np.random.multivariate_normal(mean, cov, 256))
+# Class 1: N((2,2), 2*I)
+mean1 = np.array([2, 2])
+cov1 = np.array([[2, 0], [0, 2]])  # 2 * Identity Matrix
+class1_data = np.random.multivariate_normal(mean1, cov1, 256)
 
-plt.plot(X[:,0],X[:,1], 'o', label='Individu')
+# Class 2: N((-4,-4), 6*I)
+mean2 = np.array([-4, -4])
+cov2 = np.array([[6, 0], [0, 6]])  # 6 * Identity Matrix
+class2_data = np.random.multivariate_normal(mean2, cov2, 256)
+
+# Plotting
+plt.plot(class1_data[:,0], class1_data[:,1], 'o', color='blue', label='Classe 1: N([2,2], 2I)')
+plt.plot(class2_data[:,0], class2_data[:,1], 'x', color='red', label='Classe 2: N([-4,-4], 6I)')
+
 plt.legend()
+plt.axis('equal')
+plt.grid(True)
 plt.show()
